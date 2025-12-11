684b1b2fb07e0b910dafec4e6a97193b1162b95a
Gabriele <vitoriagabriele957@gmail.com>
2025-12-11 16:03:18 -0300

    HomeDiv

--- app/page.js
+++ app/page.js
@@ -6,9 +6,14 @@ function Home() {
 
     return (
         <div>
-          <h1 style = {style}>Bem-vindo ao Souza Arquitetura</h1>
-          <p style={{color: 'lilac', backgroundColor:'soft purple'}}>No dinâmico universo da arquitetura, onde a criatividade encontra a técnica e a visão se materializa em concreto e aço, surge o Souza Ateliê Arquitetônico. Mais do que um escritório de design, o Ateliê é um espaço onde ideias florescem, conceitos se aprimoram e projetos ganham vida com uma alma única. </p>
-          <p style={{color: 'lilac', backgroundColor:'soft purple'}}>Fundado pela arquiteta Gabriele Souza, a empresa rapidamente se destacou por sua abordagem personalizada e um compromisso inabalável com a excelência.</p>
+          <header>
+            <h1 style = {style}>Bem-vindo ao Souza Arquitetura</h1>
+            <p style={{color: 'lilac', backgroundColor:'soft purple'}}>No dinâmico universo da arquitetura, onde a criatividade encontra a técnica e a visão se materializa em concreto e aço, surge o Souza Ateliê Arquitetônico.</p>
+            <p style={{color: 'lilac', backgroundColor:'soft purple'}}>Fundado pela arquiteta Gabriele Souza, a empresa rapidamente se destacou por sua abordagem personalizada e um compromisso inabalável com a excelência.</p>
+          </header>
+          <body>
+            <div class="HomeDiv"></div>           
+          </body>
 
         </div>
       );
