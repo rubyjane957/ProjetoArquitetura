1f759d383a74d2c4ab42f06979d7bf9e77506247
GABRIELE VITORIA SOARES DE SOUZA <0074466@IFMG.EDU.BR>
2025-12-11 08:39:45 -0300

    home text

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ function Home() {
     return (
         <div>
           <h1 style = {style}>Bem-vindo ao Souza Arquitetura</h1>
-          <p style={{color: 'lilac', backgroundColor:'soft purple'}}>teste</p>
+          <p style={{color: 'lilac', backgroundColor:'soft purple'}}>No dinâmico universo da arquitetura, onde a criatividade encontra a técnica e a visão se materializa em concreto e aço, surge o Souza Ateliê Arquitetônico. Mais do que um escritório de design, o Ateliê é um espaço onde ideias florescem, conceitos se aprimoram e projetos ganham vida com uma alma única. Fundado pela arquiteta Gabriele Souza, a empresa rapidamente se destacou por sua abordagem personalizada e um compromisso inabalável com a excelência.</p>
           
         </div>
       );
